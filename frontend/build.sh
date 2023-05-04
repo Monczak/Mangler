b69a0053c38a9b6b32f7c62038bf214ecd7dd82f
Monczak <szymon.jedrzejczak.sj@gmail.com>
2023-05-05 01:20:07 +0200

    Frontend: add rm safety check to build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
-rm -rf $BUILD_DIR/*
+if [[ -z "$BUILD_DIR" ]]; then
+    echo "Error: BUILD_DIR is not set"
+    exit 1
+fi
+
+if [[ "$BUILD_DIR" == "/" ]]; then
+    echo "Error: BUILD_DIR cannot be root directory"
+    exit 1
+fi
+
+if [[ "$BUILD_DIR" == "." ]]; then
+    echo "Error: BUILD_DIR cannot be current directory"
+    exit 1
+fi
+
+rm -rf "$BUILD_DIR/*"
 
 mkdir -p $BUILD_DIR
 mkdir -p $BUILD_DIR/static
