a1186cc774c813ed2c500fca8a2cfc102e83138d
Monczak <37554206+Monczak@users.noreply.github.com>
2023-05-10 17:58:16 +0200

    Backend: make api/upload work, add rate limit

--- backend/api/__init__.py
+++ backend/api/__init__.py
@@ -1,11 +1,29 @@
+import os
+import uuid
+
 import requests
 
+from pathlib import Path
+
 from flask import Blueprint, request, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
+from __main__ import app
 
 api = Blueprint("api", __name__)
+limiter = Limiter(get_remote_address, app=app, storage_uri=os.environ["REDIS_URL"])
+
+UPLOAD_DIR = Path(os.environ["UPLOADS"])
 
 
 @api.route("/upload", methods=["POST"])
+@limiter.limit("10/minute")
 def upload():
-    response = str(request.files)
-    return jsonify(response), 202
+    file_id = uuid.uuid4()
+
+    for i, (name, file) in enumerate(request.files.items()):
+        filename = f"{file_id}.{i}"
+        file.save(UPLOAD_DIR / filename)
+
+    return jsonify({"id": file_id}), 202
--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,15 @@
 import os
 
-import api
-
 from pathlib import Path
 
 from flask import Flask, render_template
-
 
 frontend_path = Path(os.environ["FRONTEND"])
 
 app = Flask(__name__, static_folder=frontend_path / "static", template_folder=frontend_path / "templates")
 app.jinja_env.add_extension("pypugjs.ext.jinja.PyPugJSExtension")
+
+import api
 
 app.register_blueprint(api.api, url_prefix="/api")
 
