c9c37a8e483c0b6fd65190e37370d83279585843
Monczak <37554206+Monczak@users.noreply.github.com>
2023-05-30 16:08:41 +0200

    Frontend: hook up file badges to file storage, rework to use IDs

--- frontend/src/ts/filestorage.ts
+++ frontend/src/ts/filestorage.ts
@@ -3,18 +3,18 @@ import { Singleton } from "utils/singleton";
 export type FileStorageUpdateCallback = (files: Array<File>) => void;
 
 export class FileStorage extends Singleton<FileStorage>() {    
-    private fileSet!: Set<File>;
+    private fileMap!: Map<string, File>;
 
     private updateCallbacks!: Set<FileStorageUpdateCallback>;
 
     constructor() {
         super();
-        this.fileSet = new Set<File>();
+        this.fileMap = new Map<string, File>();
         this.updateCallbacks = new Set<FileStorageUpdateCallback>();
     }
 
     private notifyUpdate() {
-        this.updateCallbacks.forEach(callback => callback([...this.fileSet]));
+        this.updateCallbacks.forEach(callback => callback([...this.files()]));
     } 
 
     registerCallback(callback: FileStorageUpdateCallback): void {
@@ -25,18 +25,28 @@ export class FileStorage extends Singleton<FileStorage>() {
         this.updateCallbacks.delete(callback);
     }
 
-    addFile(file: File): void {
-        // TODO: This doesn't work the way it should (separate objects), need to ID the files somehow
-        this.fileSet.add(file);
+    addFile(file: File): boolean {
+        if (this.fileMap.has(file.name))
+            return false;
+        
+        this.fileMap.set(file.name, file);
+        this.notifyUpdate();
+        return true;
+    }
+
+    removeFile(file: File): void {
+        console.log(file);
+        this.fileMap.delete(file.name);
         this.notifyUpdate();
     }
 
-    removeFile(file: File) : void {
-        this.fileSet.delete(file);
-        this.notifyUpdate();
+    removeFileByName(fileName: string): void {
+        let file = this.fileMap.get(fileName);
+        if (file)
+            this.removeFile(file);
     }
 
-    files = () => this.fileSet.entries();
+    files = () => this.fileMap.values();
 
 
 }
--- frontend/src/ts/ui/elements/filebadges.ts
+++ frontend/src/ts/ui/elements/filebadges.ts
@@ -1,31 +1,46 @@
 import { IEventHandler } from "@events/eventhandler";
 import { ElementCreator } from "@elements/elementcreator";
 import { Singleton } from "utils/singleton";
+import { FileStorage } from "filestorage";
 
 export class FileBadgeHandler extends Singleton<FileBadgeHandler>() implements IEventHandler {
-    constructor() {
-        super();
-    }
+    badgeFileNames: Map<HTMLElement, string>;
     
     parent = document.querySelector("#file-badge-container") as HTMLElement;
+
+    constructor() {
+        super();
+
+        this.badgeFileNames = new Map<HTMLElement, string>();
+    }
     
     onClick(event: MouseEvent) {
-        console.log(this);
-        const badge = (event.target as HTMLElement)?.closest(".file-badge");
+        const badge = (event.target as HTMLElement)?.closest(".file-badge") as HTMLElement;
         if (badge)
-            badge.remove();
+            this.removeBadge(badge);
     }
 
-    createNew(fileName: string) {
+    createNew(fileName: string): void {
         let elem = ElementCreator.createElement<HTMLDivElement>(this.parent, "#file-badge-template", elem => {
             const span = elem.querySelector("span");
             if (span)
                 span.textContent = fileName;
         });
         elem.addEventListener("click", event => this.onClick(<MouseEvent>event));
+
+        this.badgeFileNames.set(elem, fileName);
     }
 
-    setFrom(files: Array<File>) {
+    removeBadge(badge: HTMLElement): void {
+        let fileName = this.badgeFileNames.get(badge);
+        if (fileName) {
+            FileStorage.getInstance().removeFileByName(fileName);
+            this.badgeFileNames.delete(badge);
+            badge.remove();
+        }
+    }
+
+    setFrom(files: Array<File>): void {
         while (this.parent.firstChild)
             this.parent.removeChild(this.parent.lastChild as ChildNode);
         
@@ -34,7 +49,7 @@ export class FileBadgeHandler extends Singleton<FileBadgeHandler>() implements I
         }
     }
     
-    setupEventListeners() {
+    setupEventListeners(): void {
         document.querySelectorAll(".file-badge-close-btn").forEach(btn => {
             btn.addEventListener("click", event => this.onClick(<MouseEvent>event));
         });
